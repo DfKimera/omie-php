d1f184421c8abc8a2095d791312608773d7ea9e2
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-05-05 16:20:28 -0300

    Added TipoFrete enum

---
+++ src/Enums/TipoFrete.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * omie-php
+ * TipoFrete.php
+ *
+ * Copyright (c) LQDI Digital
+ * www.lqdi.net - 2017
+ *
+ * @author Aryel Tupinambá <aryel.tupinamba@lqdi.net>
+ *
+ * Created at: 05/05/2017, 16:19
+ */
+
+namespace DfKimera\OmiePhp\Enums;
+
+
+class TipoFrete {
+
+	const PorContaDoEmitente = "0";
+	const PorContaDoDestinatario = "1";
+	const PorcontaDeTerceiros = "2";
+	const SemFrete = "9";
+
+}
