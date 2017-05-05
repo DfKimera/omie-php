f8fa6b3901407f7a905d5f6eb28b6053ce3f1397
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-05-05 15:26:20 -0300

    Added CodigoParcela enum

---
+++ src/Enums/CodigoParcela.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * omie-php
+ * CodigoParcela.php
+ *
+ * Copyright (c) LQDI Digital
+ * www.lqdi.net - 2017
+ *
+ * @author Aryel Tupinambá <aryel.tupinamba@lqdi.net>
+ *
+ * Created at: 05/05/2017, 15:20
+ */
+
+namespace DfKimera\OmiePhp\Enums;
+
+
+class CodigoParcela {
+
+	const A_Vista = "000";
+	const Para_3_dias = "A03";
+	const Para_5_Dias = "A05";
+	const Para_7_Dias = "A07";
+	const Para_8_Dias = "A08";
+	const Para_9_Dias = "A09";
+	const Para_10_Dias = "A10";
+	const Para_13_Dias = "A13";
+	const Para_14_Dias = "A14";
+	const Para_15_Dias = "A15";
+	const Para_17_Dias = "A17";
+	const Para_20_Dias = "A20";
+	const Para_21_Dias = "A21";
+	const Para_25_Dias = "A25";
+	const Para_26_Dias = "A26";
+	const Para_28_Dias = "A28";
+	const Para_35_Dias = "A35";
+	const Para_36_Dias = "A36";
+	const Para_40_Dias = "A40";
+	const Para_42_Dias = "A42";
+	const Para_45_Dias = "A45";
+	const Para_50_Dias = "A50";
+	const Para_56_Dias = "A56";
+	const Para_60_Dias = "A60";
+	const Para_70_Dias = "A70";
+	const Para_75_Dias = "A75";
+	const Para_90_Dias = "A90";
+	const Para_98_Dias = "A98";
+	const Para_120_Dias = "B20";
+	const Em_1_Parcela_30_Dias = "001";
+	const Em_2_Parcelas = "002";
+	const Em_3_Parcelas = "003";
+	const Em_4_Parcelas = "004";
+	const Em_5_Parcelas = "005";
+	const Em_6_Parcelas = "006";
+	const Em_7_Parcelas = "007";
+	const Em_10_Parcelas = "010";
+	const Em_12_Parcelas = "012";
+	const Em_24_Parcelas = "024";
+	const Em_36_Parcelas = "036";
+	const Em_48_Parcelas = "048";
+	const D30_60 = "S01";
+	const D45_60 = "S02";
+	const D21_28_35 = "S03";
+	const D21_28_35_42 = "S04";
+	const D28_35_42 = "S05";
+	const D28_35_42_49 = "S06";
+	const D30_45_60_75_90 = "S07";
+	const D25_56 = "S08";
+	const D30_45 = "S09";
+	const D28_56 = "S10";
+	const D10_30_60 = "S11";
+	const D15_30_60 = "S12";
+	const D28_35 = "S13";
+	const D7_14_21 = "S14";
+	const D10_30_60_90 = "S15";
+	const D60_90_120 = "S16";
+	const D45_60_90 = "S17";
+	const D30_60_90 = "S18";
+	const D14_21 = "S19";
+	const D7_14 = "S20";
+	const D14_21_28 = "S21";
+	const D45_75 = "S22";
+	const D30_45_60 = "S23";
+	const D3_20_40 = "S24";
+	const D30_60_90_120 = "S25";
+	const D21_28 = "S26";
+	const A_Vista_15 = "S27";
+	const A_Vista_30 = "S28";
+	const A_Vista_30_60 = "S29";
+	const A_Vista_30_60_90 = "S30";
+	const A_Vista_30_60_90_120_150 = "S31";
+	const D28_42_56 = "S41";
+	const D15_45_75 = "S32";
+	const D14_28_42 = "S33";
+	const D14_21_28_35_42 = "S34";
+	const D30_42_54_66_78_90 = "S35";
+	const D14_21_28_35 = "S36";
+	const D28_42 = "S37";
+	//const D30_45_60 = "S38";
+	const D35_42_49_56 = "S39";
+	const D28_42_56_70 = "S40";
+	const D30_40_50_60 = "S42";
+	const D30_50_70_90 = "S43";
+	const D14_28 = "S44";
+	const D45_60_75_90 = "S45";
+	const A_Vista_30_60_90_120 = "S46";
+	const A_Vista_20_40_60 = "S47";
+	const D21_42 = "S48";
+	const D15_30_45 = "S49";
+	const D14_42 = "S50";
+	const D21_35 = "S51";
+	const D28_56_84 = "S52";
+	const D28_42_56_70_84 = "S53";
+	const A_Vista_30_45 = "S54";
+	const D21_45 = "S55";
+	const A_Vista_28 = "S56";
+	const A_Vista_60_90 = "S57";
+	const D35_45_55 = "S58";
+	const D28_35_42_56 = "S59";
+	const D30_45_60_75 = "S60";
+	const D28_35_42_49_56 = "S61";
+	const D40_70_100 = "S62";
+	const D42_56 = "S63";
+	const A_Vista_28_35 = "S64";
+	const D35_42 = "S65";
+	const D20_40 = "S66";
+	const A_Vista_28_35_42 = "S67";
+	const A_Vista_20_40_60_80 = "S68";
+	const A_Vista_20_40_60_80_100 = "S69";
+	const A_Vista_20_40_60_80_100_120 = "S70";
+	const A_Vista_30_60_90_120_150_180_210_240_270_300_330_360 = "S71";
+	const A_Vista_30_60_90_120_150_180_210_240_270_300 = "S72";
+	const D28_56_84_112 = "S73";
+	const D14_28_42_56 = "S74";
+	const D28_42_56_70_84_98 = "S75";
+	const D15_30_45_60_75 = "S76";
+	const A_Vista_15_30 = "S77";
+	const A_Vista_20_40 = "S78";
+	const D35_42_56 = "S79";
+	const D10_30_60_90_120 = "S80";
+	const D15_30_45_75_90_105_120 = "S81";
+	const D30_45_75_90_105_120 = "S82";
+	const D42_49_56 = "S83";
+	const D35_42_49 = "S84";
+	const A_Vista_60_90_120_150 = "S85";
+	const A_Vista_30_45_60 = "S86";
+	const D20_40_60_80_100 = "S87";
+	const D15_30 = "S89";
+	const D10_30_50 = "S90";
+	const D45_52_60 = "S91";
+	const D10_30 = "S92";
+	const D20_30_40_50_60_70 = "S93";
+	const D45_52_59_66 = "S94";
+	const D15_30_45_60 = "S95";
+	const D40_50 = "S96";
+	const D21_42_56 = "S97";
+	const A_Vista_60_90_120_150_180_240_300_330 = "S98";
+	const A_Vista_180 = "S99";
+	const D30_40_50 = "T01";
+	const D21_28_35_42_49 = "T02";
+	const D30_37_45_60 = "T03";
+	const A_Vista_30_60_90_120_150_180_210_240_270 = "T04";
+	const A_Vista_30_60_90_120_150_180_210 = "T05";
+	const D30_60_90_120_150_180_210_240 = "T06";
+	const D56_84_112 = "T07";
+	const D15_30_45_60_75_90_105 = "T08";
+	const D21_35_42 = "T09";
+	const D35_49 = "T10";
+	const D30_45_60_75_90_105_120 = "T11";
+	const D45_75_105_135 = "T12";
+	const D35_60_75 = "T13";
+	const D10_40_70_100_130 = "T14";
+	const D45_60_75 = "T15";
+	const D40_55_70 = "T16";
+	const D40_70 = "T17";
+	const D20_40_60 = "T18";
+	const D60_90 = "T19";
+	const D25_35_45_55 = "T20";
+	const D15_45 = "T21";
+	const D7_30_45 = "T22";
+	const D7_30_60 = "T23";
+	const D64_71 = "T24";
+	const D20_30_40 = "T25";
+	const Informar_Numero_Parcelas = "999";
+
+}
